0cf1aa43b42681fd2868e64b42345497f4914d5c
chang <chang@chang-PC>
2016-10-18 14:56:59 +0800

    Disabled the Cache on Front Page

--- WebContent/js/nemchina_explorer.js
+++ WebContent/js/nemchina_explorer.js
@@ -586,3 +586,14 @@ app.filter('to_trusted', ['$sce', function ($sce) {
 	    return $sce.trustAsHtml(text);
 	};
 }]);
+
+//disable the cache
+app.config(["$httpProvider", function($httpProvider) {
+    if( !$httpProvider.defaults.headers.get ) {
+        $httpProvider.defaults.headers.get = {};
+    }
+    //禁用缓存
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}]);
